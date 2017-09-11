ba4f0d445739d03524a0391ff8f83142baa61630
Braydon Whitmarsh <braydonwhitmarsh@me.com>
2017-09-11 23:29:07 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -4,15 +4,16 @@ class Backer
 
   attr_accessor :name, :backed_projects, :backers
 
+  BACKERS = []
+
   def initialize(name)
     @backed_projects = []
-    @backers = []
     @name = name
   end
 
   def back_project(project)
     backed_projects << project
-    backers << self
+    BACKERS << self
     binding.pry
   end
 end
