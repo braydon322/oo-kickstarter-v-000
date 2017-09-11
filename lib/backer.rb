fb2f7b08aac407cc935d5fde0a0e82ccf69bb855
Braydon Whitmarsh <braydonwhitmarsh@me.com>
2017-09-11 23:38:04 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -2,7 +2,8 @@ require 'pry'
 
 class Backer
 
-  attr_accessor :name, :backed_projects, :backers
+  attr_accessor :backed_projects, :backers
+  attr_reader :name
 
   def initialize(name)
     @backed_projects = []
