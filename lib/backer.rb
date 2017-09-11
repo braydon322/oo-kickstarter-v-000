1a7110186d2b6b5ce5e5dd0b342372da19251143
Braydon Whitmarsh <braydonwhitmarsh@me.com>
2017-09-11 23:33:40 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -6,13 +6,11 @@ class Backer
 
   def initialize(name)
     @backed_projects = []
-    @backers = []
     @name = name
   end
 
   def back_project(project)
     backed_projects << project
     project.add_backer(@name)
-    backers << self
   end
 end
