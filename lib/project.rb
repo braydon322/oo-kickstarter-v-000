0b82a2fe06928ef5cd58ae662e60d2c91b76ef7a
Braydon Whitmarsh <braydonwhitmarsh@me.com>
2017-09-11 23:31:00 +0000

    automatically backed up by learn

--- lib/project.rb
+++ lib/project.rb
@@ -4,14 +4,16 @@ class Project
 
   attr_accessor :title, :backers
 
+  
+  
   def initialize(title)
     @title = title
-    @backers = []
     @backed_projects = []
   end
 
   def add_backer(name)
     @backers << name
     @backed_projects << self
+    Backer.backers << name
   end
 end
